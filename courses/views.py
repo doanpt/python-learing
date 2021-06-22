39e877a5c173e95c231b4e5c37e30984ae7bb3f8
Trung Doan <doanpt93cntt@gmail.com>
2021-06-22 21:14:43 +0700

    django views, templates and reder data to templates

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from courses.models import Course
 
-# Create your views here.
+
+def home_view(request):
+    object_list = Course.objects.all()
+    return render(request, 'home.html', {
+        'object_list': object_list,
+        'nav': 'home'
+    })
+
+
+def about_view(request):
+    return render(request, 'about.html', {
+        'nav': 'about'
+    })
--- vnegg_web/urls.py
+++ vnegg_web/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+
+from courses import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('about/', TemplateView.as_view(template_name="home.html")),
-]
+    path('', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
