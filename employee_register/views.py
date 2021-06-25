02ea27d17a69b2e82899e002cda8137383ff9991
Trung Doan <doanpt93cntt@gmail.com>
2021-06-25 09:38:17 +0700

    create form for employee and pass it into html redered file

--- employee_register/views.py
+++ employee_register/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
+from employee_register.forms import EmployeeForm
+
 
 def employee_list(request):
     return render(request, "employee_register/employee_list.html")
 
 
 def employee_form(request):
-    return render(request, "employee_register/employee_form.html")
+    form = EmployeeForm()
+    return render(request, "employee_register/employee_form.html", {'form': form})
 
 
 def employee_delete(request):
