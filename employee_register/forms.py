dea70399b76eb2f248b67ba0b8fa8e756ad72cc0
Trung Doan <doanpt93cntt@gmail.com>
2021-06-25 09:50:39 +0700

    change lable of fields and position of fields

--- employee_register/forms.py
+++ employee_register/forms.py
@@ -7,3 +7,9 @@ class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = '__all__'
+        labels = {
+            'fullname': 'Full Name',
+            'emp_code': 'EMP. code',
+            'mobile': 'Mobile',
+            'position': 'Position'
+        }
