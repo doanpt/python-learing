d8afe00d2163bd8c60c955bc070f8bbbb8430327
Trung Doan <doanpt93cntt@gmail.com>
2021-06-25 16:11:21 +0700

    add models

--- order/models.py
+++ order/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from cart.models import Cart
+from user.models import CustomerUser
+
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomerUser, on_delete=models.CASCADE)
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    shipping_address = models.CharField(max_length=255, default='')
+    order_description = models.CharField(max_length=255, default='')
+    order_total = models.IntegerField(default=0)
+    is_completed = models.BooleanField(default=False)
